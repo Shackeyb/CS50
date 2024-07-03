88d76784f8e65962101f2bdec733cf23545e18f9
Shackeyb <Shackeyb@gmail.com>
2024-07-03 10:27:31 -0400

    This is the Indoor.py problemset

---
+++ CS50_problemsets/indoor.py
@@ -0,0 +1,7 @@
+def library():
+    print("Hello, welcome to the library please use your indoor voice")
+
+library()
+hello = input("How can I help you today ?")
+hello = hello.lower()
+print(hello)
