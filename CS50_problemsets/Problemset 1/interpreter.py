1bc9cac2dce1089ed6683e22c545bb7ee4ce6bd7
Shackeyb <Shackeyb@gmail.com>
2024-07-20 15:00:54 -0400

    Your commit message

---
+++ CS50_problemsets/Problemset 1/interpreter.py
@@ -0,0 +1,22 @@
+def main():
+    expression = input("Enter an arithmetic expression (e.g., 1 + 1): ")
+    
+    x, y, z = expression.split(" ")
+    
+    x = float(x)
+    z = float(z)
+    
+    if y == '+':
+        result = x + z
+    elif y == '-':
+        result = x - z
+    elif y == '*':
+        result = x * z
+    elif y == '/':
+        result = x / z
+    
+    print(f"{result}")
+
+if __name__ == "__main__":
+    main()
+
